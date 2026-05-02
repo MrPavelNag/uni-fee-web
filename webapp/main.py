975782b6f9879b6e3e0357da5de956e435ec9f42
Pavel Nag <pavelnag@Pavels-MacBook-Air-2.local>
2026-05-02 12:04:26 -0400

    deploy: restore riko route

--- webapp/main.py
+++ webapp/main.py
@@ -163,7 +163,7 @@ CHAIN_ID_TO_NAME = {
     81457: "blast",
 }
 
-APP_VERSION = "0.3.5"
+APP_VERSION = "0.3.6"
 APP_USER_AGENT = f"uni-fee-web/{APP_VERSION}"
 app = FastAPI(title="Uni Fee Web", version=APP_VERSION)
 
